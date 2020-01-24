e438f720928fe54c582ca5bb74998a31b5193f50
Ash Rahman <asrahman97@gmail.com>
2020-01-24 10:38:06 -0500

    finished

--- loops.py
+++ loops.py
@@ -5,5 +5,14 @@ print(item_len)
 for i in items:
     print(i)
 
-for i in range(4):
+for i in range(0,3,2):
+    print(items[i])
+
+items2 = [100,200,300,400,500,600,700]
+my_range = range(len(items), 2)
+print(my_range)
+
+items = [100, 200, 300, 400, 500, 600, 700, 800, 900, 100]
+
+for i in range(1, len(items) - 1, 2):
     print(items[i])
